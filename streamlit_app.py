0cb6e0c59b959776f096bfa16bd313723de8a1ad
Odalis Torres <odalistorrestapia@gmail.com>
2023-07-17 16:04:58 -0600

    Update streamlit_app.py
    Added pandas library to read CSV file 
    Added a dataframe to display the fruit list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,12 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
+## Read csv file
+## read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+## After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page
+streamlit.dataframe(my_fruit_list)
