f4159bbc0d82efcf647ea6b139e7d6f24f7f05b0
Odalis Torres <odalistorrestapia@gmail.com>
2023-07-17 16:06:36 -0600

    Update streamlit_app.py
    added pandas import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -12,7 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-
+import pandas
 ## Read csv file
 ## read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
