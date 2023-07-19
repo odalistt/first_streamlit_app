5c4fda7037dd16778554e7f647cc9c77749a85c6
Odalis Torres <odalistorrestapia@gmail.com>
2023-07-19 09:40:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   # Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-      if not fruit_choice:
-          streamlit.error("Please select a fruit to get information.")
-      else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
       streamlit.error()
